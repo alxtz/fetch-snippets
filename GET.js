056d0448755d8718942fe632e1f73d7fe23c0871
Alex Tzeng <alxtz.tw@gmail.com>
2017-09-02 20:31:58 +0800

    add json GET

--- GET.js
+++ GET.js
@@ -6,3 +6,9 @@ fetch(url)
   .then( function(myBlob) {
     console.log(myBlob)
   })
+
+// json 
+url = 'https://staging-api.emq.com/api/v4/transfer/corridors/HKG/PHL'
+fetch( url )
+  .then( response => response.json() )
+  .then( json => console.log(json) )
